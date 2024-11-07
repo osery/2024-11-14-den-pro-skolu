0f82c9bdbf9c7ac3f8a4b1c04229a27b416f053d
Ondrej Sery <ondrej.sery@eyetowers.cz>
2024-11-07 16:12:54 +0100

    Hello world server

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	http.HandleFunc("GET /", helloWorld)
+	http.ListenAndServe(":8080", nil)
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world!")
 }
