339a7f12a75f50e09680a333374eca5ed0b036fb
Ondrej Sery <ondrej.sery@eyetowers.cz>
2024-11-07 16:09:30 +0100

    Hello world console

--- main.go
+++ main.go
@@ -2,25 +2,8 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 )
 
-var messages = []string{
-	"zprava 1",
-	"zprava 2",
-}
-
 func main() {
-	http.HandleFunc("GET /", listMessages)
-
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func listMessages(w http.ResponseWriter, r *http.Request) {
-	for _, m := range messages {
-		fmt.Fprintf(w, "%s\n", m)
-	}
+	fmt.Println("Hello world!")
 }
